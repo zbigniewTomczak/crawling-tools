50920d3737cb9b864e69ed455506e98f415e2aa6
Zbigniew Tomczak, Comp SA <zbigniew.tomczak@comp.com.pl>
2013-01-16 09:55:01 +0100

    Test response code 200 from address list in file code.200

---
+++ src/test/java/ResponseCodeTest.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ResponseCodeTest {
+	
+	@Test
+	public void fileCode200() throws IOException {
+		final String fileName = "target/test-classes/code.200";
+		final String codeOk = "200";
+		File file = new File(fileName);
+		if (! file.exists() ) {
+			Assert.fail(String.format("File %s does not exist.",file.toString()));
+		}
+		BufferedReader reader = new BufferedReader(new FileReader(file));
+		String line = null;
+		boolean fail = false;
+		while ((line = reader.readLine()) != null) {
+			String codeReturn = ResponseCode.responseCode(line);
+			if (! codeReturn.equals(codeOk)) {
+				System.out.format("%s %s", line, codeReturn);
+				fail = true;
+			}
+		}
+		if (fail) {
+			Assert.fail("Not all addresses returned status: " + codeOk + ". See console log for detail.");
+		}
+	}
+}
